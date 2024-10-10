c65df231e5effc33c883d29d787bbfd2c967a21d
mjunaid46 <muhammad.junaid@zortik.com>
2024-10-10 12:35:47 +0500

    Update test.c

--- test.c
+++ test.c
@@ -44,3 +44,21 @@ int for (int i = 0; i < x; i++)
     malloc(size * sizeof(int));
     return 0;
 }
+
+ // Function declarations
+
+    int x = 10;
+    for (int i = 0; i < x; i++) {
+        global_var += i;
+    }
+
+    int *arr = malloc(x * sizeof(int)); 
+    
+  
+
+    for (int i = 0; i < x; i++) {
+        arr[i] = i * 2;
+    }
+
+   
+}
